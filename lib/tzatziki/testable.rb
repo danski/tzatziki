cf3aa60eccb1743c4bab8cad588a6212247e866f
danski <dan@angryamoeba.co.uk>
2009-04-01 02:10:00 +0100

    stubbing out Tzatziki::Testable with defaults hashes

--- lib/tzatziki/testable.rb
+++ lib/tzatziki/testable.rb
@@ -2,6 +2,28 @@ module Tzatziki
   
   module Testable
     
+    class << self
+      # Singleton methods
+      
+      def request_defaults
+        {
+          :domain=>"http://localhost",
+          :uri=>"/",
+          :query_string=>{},
+          :post_body=>{},
+          :headers=>{
+            :"content-accept"=>"application/xml"
+          }
+        }
+      end
+      
+      def response_defaults
+        { 
+          :status=>200
+        }
+      end
+    end
+    
   end
   
 end
--- spec/testable_spec.rb
+++ spec/testable_spec.rb
@@ -1,8 +1,27 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe Tzatziki::Testable do
+  
+  before(:all) do
+    class ::TestTestable
+      include Tzatziki::Testable
+    end
+  end
+  before(:each) do
+    @testable = ::TestTestable.new
+  end
 
-  it "can only be included in parsable objects"
+  it "should provide defaults for the request object" do
+    Tzatziki::Testable.request_defaults.should be_kind_of(Hash)
+  end
+  it "should provide defaults for the response assertions" do
+    Tzatziki::Testable.response_defaults.should be_kind_of(Hash)
+  end
+  
+  it "should set up a request factory when given a descriptive hash"
+  
+  it "should include included specifications into the request factory options at the appropriate level"
+  it "should include included types into the request factory options at the appropriate level"
 
   it "should manufacture requests based on the data found in the parsable hash"
   it "should manufacture response assertions based on the data found in the parsable hash"
